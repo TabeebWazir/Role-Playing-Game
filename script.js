3bac9f9522f71a51586f73680fe54844064d786a
TabeebWazir <readingworld2@gmail.com>
2024-04-27 20:22:39 -0400

    Use querySelector() to get the other two button elements using their ids: button2 and button3. Store them in variables called button2 and button3. Remember to use const.

--- script.js
+++ script.js
@@ -13,3 +13,5 @@ let inventory = ["stick"];
 let monsterHealth;
 
 const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
